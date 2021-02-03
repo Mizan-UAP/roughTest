3e20790eef3e01576382e3b2a22c48e7ec0f5b1b
Mizan-Primex <mizan@primex-bd.com>
2021-02-03 17:32:03 +0600

    first commit

---
+++ src/Component/Heaader/Heaader.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import './Heaader.css'
+
+const Heaader = () => {
+    return (
+        <div>
+            <div className="body">
+                <div className="content">
+                    <div className="logo">
+                        <div className="logoBody">
+                            <h1>Logo</h1>
+                        </div>
+                    </div>
+                    <div className="buttonItems">
+                        <div className="buttonBody">
+                            <div className="buttonContent">
+                               <a>
+                                   heeyy
+                               </a>
+                            </div>
+                        </div>
+                        <div className="buttonBody">
+                            <div className="buttonContent">
+                               <a>
+                                   heeyy1
+                               </a>
+                            </div>
+                        </div>
+                        <div className="buttonBody">
+                            <div className="buttonContent">
+                               <a>
+                                   heeyy2
+                               </a>
+                            </div>
+                        </div>
+                        <div className="buttonBody">
+                            <div className="buttonContent">
+                               <a>
+                                   heeyy3
+                               </a>
+                            </div>
+                        </div>
+                        <div className="buttonBody">
+                            <div className="buttonContent">
+                               <a>
+                                   heeyy4
+                               </a>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Heaader;
---
+++ src/Component/Home/Home.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Heaader from '../Heaader/Heaader';
+import './Home.css'
+const Home = () => {
+    return (
+        <div>
+            {/* <div class="button" id="button-5">
+                <div id="translate"></div>
+                <a href="#">Let's Go!</a>
+            </div>
+
+            <div className="test">
+
+                <div className="buttonItem">
+                    Catch
+                </div>
+
+            </div>
+            <div className="test1">
+                <label> heeeyyyy</label>
+            </div>
+            <div className="test1">
+                <label> heeeyyyy</label>
+            </div> */}
+
+            <Heaader/>
+
+
+
+        </div>
+    );
+};
+
+export default Home;
